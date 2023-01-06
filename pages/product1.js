768474f5e1d863e40f51c559f327ba6e8f2425ef
Gabianchini <gabibisdx@gmail.com>
2023-01-06 16:51:06 -0300

    Added: Create component productpage -  and use data from contentful - desktop version

--- pages/product1.js
+++ pages/product1.js
@@ -1,7 +1,6 @@
-
+import stylesProduct1 from '../styles/Product1.module.css'
 //var contentful = require("contentful") // outra forma de importação 
 import * as contentful from "contentful"
-import { PHASE_PRODUCTION_SERVER } from "next/dist/shared/lib/constants";
 console.log("contentful:", contentful)
 
 var client = contentful.createClient({
@@ -10,23 +9,44 @@ var client = contentful.createClient({
   });
 
 
+export default function ProductPage({/*heading,subheading,*/authorImg, feedbackText,stars,authorName,companyText}){
 
-export default function ProductPage({heading,subheading}){
-
-    return <>
-    <h1>{heading}</h1>
-    <h2>{subheading}</h2>
-    </>
+    return (
+    <div className={stylesProduct1.feedbackContainer}>
+   {/* <h1>{heading}</h1>
+    <h2>{subheading}</h2> */}
+    <img className={stylesProduct1.stars} alt="stars" src={stars}/>
+    <p className={stylesProduct1.feedbackText}>{feedbackText}</p>
+    <div className={stylesProduct1.description}>
+    <img className={stylesProduct1.authorImg} alt="some alt text" src={authorImg}/>
+    <h4 className={stylesProduct1.authorName}>{authorName}</h4>
+    <p className={stylesProduct1.companyText}>{companyText}</p>
+    </div>
+    </div>
+    )
 }
+    
 
 export async function getStaticProps(){
     //get data from headless cms
-   const product = await client.getEntry('3i1SIE0xxuisV56pENWULY')
+    const feedback = await client.getEntries({content_type: "feedback"})
+  // const product = await client.getEntry('3i1SIE0xxuisV56pENWULY')
     return{
             props:{
-            ...product.fields
-            }, 
+                authorImg:feedback.items[0].fields.authorImg.fields.file.url,
+                feedbackText:feedback.items[0].fields.avaliationText,
+
+                stars: feedback.items[0].fields.stars.fields.file.url,
+
+                authorName: feedback.items[0].fields.authorName,
+                
+                companyText: feedback.items[0].fields.companyText,
+            
+
+            }
+           
         }
+        
     }
 
     //will be passed to the page component as props
