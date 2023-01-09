70bad3ca99b76f7fc9e1d813c276ac87b0c0691b
Gabianchini <gabibisdx@gmail.com>
2023-01-09 14:16:27 -0300

    Refactor:components name

--- pages/product1.js
+++ pages/product1.js
@@ -5,7 +5,9 @@ console.log("contentful:", contentful)
 import Img from '../components/atoms/Img';
 import Text from '../components/atoms/Text';
 import Title from '../components/atoms/Title';
-import Subtitle from '../components/atoms/Subtitle';
+import Subtitle from '../components/atoms/Subtitle'
+
+
 
 var client = contentful.createClient({
     space: process.env.CONTENTFUL_SPACE_ID,
@@ -19,7 +21,7 @@ var client = contentful.createClient({
 
     return{
             props:{
-                response:feedback.items
+                data:feedback.items
             }
            
         }
@@ -27,35 +29,23 @@ var client = contentful.createClient({
     }
 
 
-export default function ProductPage({response}){
+export default function Feedback({data}){
   
   return(
-  response.map((item) => (
+    
+  data.map((item) => (
     <>
-    <div className={stylesProduct1.feedbackContainer} >
-      <Img className={stylesProduct1.stars} alt="stars" src={item.fields.stars.fields.file.url}/>
-      <Text className={stylesProduct1.feedbackText}>{item.fields.avaliationText}</Text>
-    <div className={stylesProduct1.description}>
-      <Img className={stylesProduct1.authorImg} alt="some alt text" src={item.fields.authorImg.fields.file.url}/>
-      <Title className={stylesProduct1.authorName}>{item.fields.authorName}</Title>
-      <Subtitle className={stylesProduct1.companyText}>{item.fields.companyText}</Subtitle>
+    <div className={stylesProduct1.feedback__Container} key={item.fields.authorName} >
+      <Img className={stylesProduct1.feedback__Stars} alt="stars" src={item.fields.stars.fields.file.url}/>
+      <Text className={stylesProduct1.feedback__Text}>{item.fields.avaliationText}</Text>
+    <div className={stylesProduct1.feedback__description}>
+      <Img className={stylesProduct1.feedback__authorImg} alt="some alt text" src={item.fields.authorImg.fields.file.url}/>
+      <Title className={stylesProduct1.feedback__authorName}>{item.fields.authorName}</Title>
+      <Subtitle className={stylesProduct1.feedback__companyText}>{item.fields.companyText}</Subtitle>
     </div>
     </div>
-      <hr/>
+      
     </>
   ))
   )
   }
-
-
- 
-
-
-
-
-
-    
-
-
-    //will be passed to the page component as props
-    
