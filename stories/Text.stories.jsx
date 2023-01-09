fb8c352524edd6c2a1b7c4662b5a98e4c740392a
Gabianchini <gabibisdx@gmail.com>
2023-01-09 14:54:08 -0300

    Creating 1 component in storybook

---
+++ stories/Text.stories.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+
+
+
+export default{
+    title:"FeedbackText",
+    component:"FeedbackText"
+};
+
+export const FeedbackText = (className,children) => <p className={className}>Teste</p>;
